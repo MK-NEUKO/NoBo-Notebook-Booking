2258eb77b017fd03dc39d872758d15615004c002
Michael Koch <m.koch@neuko-devment.com>
2024-12-05 16:10:53 +0100

    Add logging and DI support with MediatR enhancements
    Updated NoBo.Application.csproj to include Microsoft.Extensions.Logging.Abstractions package for logging support. Added DependencyInjection.cs to configure MediatR and register services, including a transient PricingService. Introduced LoggingBehavior.cs to implement logging behavior in the MediatR pipeline using ILogger.

---
+++ src/NoBo.Application/Abstractions/Behavior/LoggingBehavior.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NoBo.Application.Abstractions.Messaging;
+
+namespace NoBo.Application.Abstractions.Behavior;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IBaseCommand
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public LoggingBehavior(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        var name = request.GetType().Name;
+
+        try
+        {
+            _logger.LogInformation("Executing Command {Command}", name);
+
+            var result = await next();
+
+            _logger.LogInformation("Command {Command} progressed successfully", name);
+
+            return result;
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Command {Command} progressing failed", name);
+            throw;
+        }
+    }
+}
---
+++ src/NoBo.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NoBo.Application.Abstractions.Behavior;
+using NoBo.Domain.Bookings;
+
+namespace NoBo.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+            configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
