738dc73b2f382dcb0a21896175e38de8eb63d558
Michael Koch <m.koch@neuko-devment.com>
2024-12-05 08:39:48 +0100

    Update target framework to .NET 9.0
    The target framework for the project has been updated from .NET 8.0 to .NET 9.0. This change is reflected in the <TargetFramework> element within the <PropertyGroup> section of the NoBo.Domain.csproj file.
    
    Add NoBo.Application project and update solution
    
    Updated NoBo.sln to include the new NoBo.Application project located at src\NoBo.Application\NoBo.Application.csproj. Modified NoBo.Domain.csproj to target net9.0. Added NoBo.Application.csproj targeting net9.0 with a package reference to MediatR 12.4.1 and a reference to NoBo.Domain.
    
    Added new interface files:
    - ICommand.cs: Defines ICommand and ICommand<TResponse> interfaces.
    - ICommandHandler.cs: Defines ICommandHandler<TCommand> and ICommandHandler<TCommand, TResponse> interfaces.
    - IQuery.cs: Defines IQuery<TResponse> interface.
    - IQueryHandler.cs: Defines IQueryHandler<TQuery, TResponse> interface.

---
+++ src/NoBo.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using NoBo.Domain.Abstractions;
+
+namespace NoBo.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+{
+}
+
+public interface IBaseCommand
+{
+}
